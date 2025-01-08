278fd7a2a0f03ceeb38f44f6741adac1ddcecb51
Deniel Salvacion <denielsalvacion@gmail.com>
2025-01-09 05:49:41 +0800

    feat: add clearFavorites in link root route

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 import { useFavorites } from "../context/FavoriteContext";
 
 const Header = () => {
-  const { favoriteData } = useFavorites();
+  const { favoriteData, clearFavorites } = useFavorites();
 
   return (
     <header className="border-b border-outline fixed top-0 z-50 bg-white w-full">
       <nav className="w-full mx-auto flex items-center justify-between flex-wrap px-4 py-3">
         <div className="flex justify-center items-center gap-2">
           <h1 className="text-xl text-[#0077D4] font-bold">
-            <Link to="/">Supermarket Product</Link>
+            <Link to="/" onClick={() => clearFavorites()}>
+              Supermarket Product
+            </Link>
           </h1>
         </div>
         <div className="flex justify-center items-center">
@@ -20,7 +22,7 @@ const Header = () => {
             <li>
               <Link to="/favorites">
                 Favorites{" "}
-                <span className=" text-yellow-500 ">
+                <span className=" text-yellow-500">
                   {favoriteData.length > 0 ? favoriteData.length : ""}
                 </span>
               </Link>
